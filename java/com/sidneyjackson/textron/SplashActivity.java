2b99e8cb44401c4bf0470e1dd811b87099beba6b
Sidney Jackson <werty6400@gmail.com>
2017-10-25 22:57:58 -0400

    Added a Splash Screen (now set to first opening activity) and LoginActivity using the preset android studio Login Java file. Notes: Need to work on cleaning up the transition from login to MainActivity. Also need to add simple registration activity and integrate an actual database (FireBase probably?) that can reference users. After this, consider adding GMail login as well.

---
+++ java/com/sidneyjackson/textron/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.sidneyjackson.textron;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_TIME = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startSplashTimer();
+    }
+
+    private void startSplashTimer() {
+        try {
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, SPLASH_TIME);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
