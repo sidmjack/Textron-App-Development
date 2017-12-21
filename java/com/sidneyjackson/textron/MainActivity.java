30fd950c7e7a0c8c6658b4865ca4084a9a1cbc79
Sidney Jackson <werty6400@gmail.com>
2017-12-20 22:34:27 -0500

    Improved Error handling with BT Connections. Is now also able to reconnect with lost BT connections.

--- java/com/sidneyjackson/textron/MainActivity.java
+++ java/com/sidneyjackson/textron/MainActivity.java
@@ -4,7 +4,10 @@ import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.AsyncTask;
@@ -20,6 +23,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
@@ -38,41 +42,45 @@ public class MainActivity extends AppCompatActivity {
     private Set<BluetoothDevice> pairedDevices; //Set of Bluetooth Devices
     private BluetoothAdapter mBluetoothAdapter; // Device Bluetooth Adapter
     public String currAddress = "";
-    private HashMap<String, String> btHashmap = new HashMap<String, String>();
+    public String connectedAddress = "";
+
+    private HashMap<String, String> btHashmap_paired = new HashMap<String, String>();
+    private HashMap<String, String> bt_Hashmap_discovered = new HashMap<String, String>();
+    private ArrayList<BluetoothDevice> btDeviceList = new ArrayList<BluetoothDevice>();
+
+    //final ArrayList discover_list = new ArrayList();
+
     public boolean bt_connection_established = false;
 
-    ListView deviceList; // List of Bluetooth Devices
-    Button deviceListBtn; // Button to View List of Bluetooth Devices
+    ListView deviceListPaired; // List of Bluetooth Devices
+    ListView deviceListScanned; // List of Bluetooth Devices
+    Button deviceListPairedBtn; // Button to View List of Past Bluetooth Devices
+    Button deviceListScanBtn; // Button to View List of all Discovered Bluetooth Devices
     Button btnUnlock; // Unlock SmartBox Button
     Button btnLock; // Lock SmartBox Button
     Button btnDisconnect; // Disconnect from Bluetooth Device Button
 
-    // TODO: Check if this is a good place for this.
-    /*Runnable myRunner = new Runnable(){
-        public void run() {
-            new ConnectBT ().execute();
+    public final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                btDeviceList.add(device);
+            }
         }
-    };*/
-
-    // TODO: Removed Temporarily.
-    //private ConnectBT cbt;
-
+    };
 
     private ProgressDialog progress;
     BluetoothSocket btSocket = null;
     private boolean isBtConnected = false;
-    //static final UUID myUUID = UUID.fromString("19706a80-b858-11e7-8f1a-0800200c9a66"); //generated for me
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //given
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: Removed Temporarily.
-        //cbt = new ConnectBT();
-
-        /* Bluetooth Adapter Object that represents the device's own Bluetooth Adapter -
-        needed for all Bluetooth Activities...*/
+        /* Bluetooth Adapter Object that represents the device's own Bluetooth Adapter - needed for all Bluetooth Activities...*/
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
             // Device does not support Bluetooth...
@@ -81,23 +89,47 @@ public class MainActivity extends AppCompatActivity {
         } else if (!mBluetoothAdapter.isEnabled()) { // If Bluetooth isn't enable, request user to enable it.
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            // Register the broadcast receiver
+            //IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            //registerReceiver(mReceiver, filter);
         }
 
         /* Initialize Bluetooth Device List and Buttons */
 
         btnUnlock = (Button)findViewById(R.id.unlock_button_id);
         btnLock = (Button)findViewById(R.id.lock_button_id);
-        deviceListBtn = (Button)findViewById(R.id.view_bt_pair_button_id);
-        deviceList = (ListView)findViewById(R.id.listView);
+
+        deviceListPairedBtn = (Button)findViewById(R.id.view_bt_pair_button_id);
+        deviceListPaired = (ListView)findViewById(R.id.listView_paired_items);
+
+        //deviceListScanBtn = (Button)findViewById(R.id.view_bt_scan_button_id);
+        //deviceListScanned = (ListView)findViewById(R.id.listView_scanned_items);
+
         btnDisconnect = (Button)findViewById(R.id.disconnect_button_id);
 
-        deviceListBtn.setOnClickListener(new View.OnClickListener() {
+        // Displays formerly paired devices.
+        deviceListPairedBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
-                pairedDevicesList(); //method that will be called
+                // Populates List of Formerly Paired Devices.
+                pairedDevicesList();
             }
         });
+
+        // Displays currently discovered devices.
+        /*deviceListScanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if (mBluetoothAdapter.isDiscovering()) {
+                    mBluetoothAdapter.cancelDiscovery();
+                }
+                mBluetoothAdapter.startDiscovery();
+
+                discoveredDevicesList();
+            }
+        });*/
 
         btnUnlock.setOnClickListener(new View.OnClickListener()
         {
@@ -106,8 +138,7 @@ public class MainActivity extends AppCompatActivity {
             {
                 btnUnlock.setBackgroundColor(getResources().getColor(colorPrimaryDark));
                 btnLock.setBackgroundColor(getResources().getColor(colorPrimary));
-                // TODO: Add Check for Connected BT Device and Helpful Message
-                turnOnLed();      //Method to turn LED on (Simulating Unlock Feature)
+                turnOnLed(); //Method to turn LED on (Simulating Unlock Feature)
             }
         });
 
@@ -117,8 +148,7 @@ public class MainActivity extends AppCompatActivity {
             {
                 btnUnlock.setBackgroundColor(getResources().getColor(colorPrimary));
                 btnLock.setBackgroundColor(getResources().getColor(colorPrimaryDark));
-                // TODO: Add Check for Connected BT Device and Helpful Message
-                turnOffLed();   //Method to turn LED off (Simulating Lock Feature)
+                turnOffLed(); //Method to turn LED off (Simulating Lock Feature)
             }
         });
 
@@ -128,14 +158,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v)
             {
                 ((TextView)findViewById(R.id.connected_bt_device_name_id)).setText("No BlueTooth Device Connected.");
-                Disconnect(); //close connection
+                Disconnect(); //Close BT Connection
                 bt_connection_established = false; // Cause app to crash
             }
         });
 
         buttonEffect(btnDisconnect);
-        buttonEffect(deviceListBtn);
-
+        buttonEffect(deviceListPairedBtn);
+        //buttonEffect(deviceListScanBtn);
     }
 
     // Methods:
@@ -150,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
             for(BluetoothDevice bt : pairedDevices)
             {
                 list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
-                btHashmap.put(bt.getAddress(), bt.getName());
+                btHashmap_paired.put(bt.getAddress(), bt.getName());
             }
         }
         else
@@ -159,12 +189,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
-        deviceList.setAdapter(adapter);
-        deviceList.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
+        deviceListPaired.setAdapter(adapter);
+        deviceListPaired.setOnItemClickListener(myListClickListener_paired); //Method called when the device from the list is clicked
 
     }
 
-    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener()
+    /*private void discoveredDevicesList()
+    {
+        ArrayList list = new ArrayList();
+
+        if (btDeviceList.size()>0)
+        {
+            for(BluetoothDevice bt : btDeviceList)
+            {
+                list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
+                bt_Hashmap_discovered.put(bt.getAddress(), bt.getName());
+            }
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "No Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        deviceListScanned.setAdapter(adapter);
+        deviceListScanned.setOnItemClickListener(myListClickListener_discovered); //Method called when the device from the list is clicked
+    }*/
+
+    // On CLick List Item Listener for Formerly Paired Devices.
+    private AdapterView.OnItemClickListener myListClickListener_paired = new AdapterView.OnItemClickListener()
     {
         public void onItemClick (AdapterView av, View v, int arg2, long arg3)
         {
@@ -174,18 +227,40 @@ public class MainActivity extends AppCompatActivity {
             currAddress = address;
 
             if (bt_connection_established == false) {
-                // TODO: Removed Temporarily and add Task Runner Here.
                 myRunner.run();
-                //cbt.execute(); // TODO: Hopefully this works?
-                bt_connection_established = true;
+            } else {
+               String message = "Already Paired with a Device";
+               msg(message);
             }
-
-            // Update text field with the name of the Bluetooth Device connected to.
-            ((TextView)findViewById(R.id.connected_bt_device_name_id)).setText("Connected to: " + btHashmap.get(currAddress));
         }
     };
 
-    // PROBLEM CHILD AS OF OCTOBER 23, 2017
+    // On CLick List Item Listener for Currently Discovered Devices.
+    /*private AdapterView.OnItemClickListener myListClickListener_discovered = new AdapterView.OnItemClickListener()
+    {
+        public void onItemClick (AdapterView av, View v, int arg2, long arg3)
+        {
+            // Get the device MAC address, the last 17 chars in the View
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+            currAddress = address;
+
+            if (bt_connection_established == false) {
+                myRunner.run();
+            } else {
+                String message = "Already Paired with a Device";
+                msg(message);
+            }
+        }
+    };*/
+
+    // //////////////////////////////////
+    // //////////////////////////////////
+    // DONT NEED TO FOCUS ON RIGHT NOW
+    // //////////////////////////////////
+    // //////////////////////////////////
+
+    // Establishes Bluetooth Connection:
     private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
     {
         private boolean ConnectSuccess = true; //if it's here, it's almost connected
@@ -202,14 +277,14 @@ public class MainActivity extends AppCompatActivity {
             try
             {
                 // TODO: Add these comments back in eventually.
-                //if (btSocket == null || !isBtConnected)
-                //{
+                if (btSocket == null || !isBtConnected)
+                {
                     mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
                     BluetoothDevice dispositivo = mBluetoothAdapter.getRemoteDevice(currAddress);//connects to the device's address and checks if it's available
                     btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
                     BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
                     btSocket.connect();//start connection
-                //}
+                }
             }
             catch (IOException e)
             {
@@ -225,33 +300,75 @@ public class MainActivity extends AppCompatActivity {
             if (!ConnectSuccess)
             {
                 msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
+                isBtConnected = false;
+                bt_connection_established = false;
+                ((TextView)findViewById(R.id.connected_bt_device_name_id)).setText("No BlueTooth Device Connected.");
+                //connectedAddress = "No Device Connected.";
                 //finish();
             }
             else
             {
                 msg("Connected.");
                 isBtConnected = true;
+                bt_connection_established = true;
+                if (btHashmap_paired.get(currAddress) != null) {
+                    ((TextView)findViewById(R.id.connected_bt_device_name_id)).setText("Connected to: " + btHashmap_paired.get(currAddress));
+                    connectedAddress = btHashmap_paired.get(currAddress);
+                }
+
+                if(btSocket != null)
+                {
+                    try
+                    {
+                        btSocket.getOutputStream().write("InitBTConnection".getBytes(Charset.forName("UTF-8")));
+                    }
+                    catch (IOException e)
+                    {
+                        msg("Error");
+                    }
+                }
+
+                /*else if (bt_Hashmap_discovered.get(currAddress) != null){
+                    ((TextView)findViewById(R.id.connected_bt_device_name_id)).setText("Connected to: " + bt_Hashmap_discovered.get(currAddress));
+                    //connectedAddress = bt_Hashmap_discovered.get(currAddress);
+                }*/
+
             }
             progress.dismiss();
         }
     }
-    // PROBLEM CHILD ENDS HERE...
 
+    // Displays Long Toasts:
     private void msg(String s)
     {
         Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
     }
 
+    // Disconnects Established BT Connection
     private void Disconnect()
     {
         if (btSocket!=null) //If the btSocket is busy
         {
             try
             {
-                btSocket.close(); //close connection
+                byte[] buffer = new byte[256];  // buffer store for the stream
+                int bytes; // bytes returned from read()
+
+                btSocket.getOutputStream().write("Disconnect010101".getBytes(Charset.forName("UTF-8")));
+                bytes = new DataInputStream(btSocket.getInputStream()).read(buffer);
+                String readMessage = new String(buffer, 0, bytes);
+                if (readMessage.contains("BtDisconnectACKD")) {
+                    btSocket.close(); //close connection
+                    isBtConnected = false;
+                    bt_connection_established = true;
+                    //connectedAddress = "";
+                    msg("BlueTooth Disconnected");
+                }
             }
             catch (IOException e)
-            { msg("Error");}
+            {
+                msg("Error");
+            }
         }
     }
 
@@ -292,8 +409,12 @@ public class MainActivity extends AppCompatActivity {
             }
             catch (IOException e)
             {
-                msg("Error");
+                if(bt_connection_established == false) {
+                    msg("No BlueTooth Device Connected");
+                }
             }
+        } else {
+            msg("No BlueTooth Device Connected");
         }
     }
 
@@ -304,6 +425,7 @@ public class MainActivity extends AppCompatActivity {
             try
             {
                 // TODO: Change Button Colors: Potential Bug/Error - Double Check sequential pairing.
+                // TODO: Attempt using the end-line and trim() functions to get this to work.
                 //btSocket.getOutputStream().write("TO".toString().getBytes()); // Formerly "TO"
                // btSocket.getOutputStream().write('1'); // Formerly "TF" 11/9/17
 
@@ -334,8 +456,12 @@ public class MainActivity extends AppCompatActivity {
             }
             catch (IOException e)
             {
-                msg("Error");
+                if(bt_connection_established == false) {
+                    msg("No BlueTooth Device Connected");
+                }
             }
+        } else {
+            msg("No BlueTooth Device Connected");
         }
     }
 
@@ -361,6 +487,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //Creates a new instance of ConnectBT in order to run a fresh BT Connection Thread
     Runnable myRunner = new Runnable(){
         public void run() {
             new ConnectBT ().execute();
